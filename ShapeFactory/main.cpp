5a71f855e5ef65edd521f63e44c296cbe5c7ea91
Gustavo Barros da Silveira <guga082011@gmail.com>
2024-11-30 17:54:36 -0300

    Initial Commit

---
+++ ShapeFactory/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ShapeFactory
+//
+//  Created by Gustavo Barros da Silveira on 30/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
